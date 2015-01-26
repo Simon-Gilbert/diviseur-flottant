df374353c5901a8a178113c2140c11f1b99cc9e7
Simon-Gilbert <simon.gilbert@bts-malraux.net>
2015-01-26 14:01:49 +0100

    first commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main () {
+
+float nb1, nb2;
+
+cout << "Veuillez entrer le premier nombre : " << endl;
+cin >> nb1;
+
+cout << "Veuillez entrer le deuxieme nombre : " << endl;
+cin >> nb2;
+
+cout << "La division des nombres " << nb1 << " par " << nb2 << " est égal a " << nb1/nb2 << endl;
+
+return 0;
+
+}
